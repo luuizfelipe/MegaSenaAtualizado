9bc9c1242a78aa1a0b3f1e95d95200a67ddef4d0
luuizzfelipe <luiznacimento95@hotmail.com>
2022-06-25 16:32:10 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import qs from 'qs';
-import { FormEvent, ReactChild, ReactFragment, ReactPortal, useState } from 'react';
+import {useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput, ScrollView, Linking } from 'react-native';
-import gameServiceDatabase from './gameServiceDatabase';
+import jogoDatabase from './JogoDatabase';
 
 export default function App() {
 
-  const [numberOfGames, setNumberOfGames] = useState<number>(0);
-  const [generatedNumbers, setGeneratedNumbers] = useState<any>([]);
-  const [maxNumber, setMaxNumber] = useState<number>(60);
-  const [emailTo, setEmailTo] = useState<string>('');
-  const [sortedValues, setSortedValues] = useState<any>(
+  const [numeroGames, setnumeroGames] = useState<number>(0);
+  const [geradorNumeros, setgeradorNumeros] = useState<any>([]);
+  const [maxNumero, setmaxNumero] = useState<number>(60);
+  const [email, setemail] = useState<string>('');
+  const [sorteioValor, setsorteioValor] = useState<any>(
     {
       value0: '',
       value1: '',
@@ -22,39 +22,39 @@ export default function App() {
   );
 
 
-  function handleClickSorterButton(numberOfGames: number) {
+  function clickButton(numeroGames: number) {
     let auxArray = []
     let list = [];
-    for (let numberOfTimesToRun = 0; numberOfTimesToRun < numberOfGames; numberOfTimesToRun++) {
+    for (let vxExecutado = 0; vxExecutado < numeroGames; vxExecutado++) {
       list = [];
-      let randomNumber;
+      let NumeroAleatorio;
       let tmp;
 
-      for (let i = 0; i < maxNumber; i++) {
+      for (let i = 0; i < maxNumero; i++) {
         list[i] = i + 1;
       }
 
       for (let i = list.length; i;) {
-        randomNumber = Math.random() * i-- | 0;
-        tmp = list[randomNumber];
-        // troca o número aleatório pelo atual
-        list[randomNumber] = list[i];
-        // troca o atual pelo aleatório
+        NumeroAleatorio = Math.random() * i-- | 0;
+        tmp = list[NumeroAleatorio];
+        
+        list[NumeroAleatorio] = list[i];
+       
         list[i] = tmp;
       }
 
       auxArray.push(list);
     }
 
-    saveGamesOnDatabase(auxArray);
-    setGeneratedNumbers(auxArray);
+    salvarJogo(auxArray);
+    setgeradorNumeros(auxArray);
   }
 
   async function sendEmail(to: any, subject: any, body: any) {
 
     let url = `mailto:${to}`;
 
-    // Create email link query
+    
     const query = qs.stringify({
       subject: subject,
       body: body,
@@ -64,41 +64,41 @@ export default function App() {
       url += `?${query}`;
     }
 
-    // check if we can use this link
+   
     const canOpen = await Linking.canOpenURL(url);
 
     if (!canOpen) {
-      throw new Error('Provided URL can not be handled');
+      throw new Error('Url não entrontrada');
     }
 
     return Linking.openURL(url);
   }
 
-  function functionTransformObject() {
-    let mountedBody = ``;
+  function transObjeto() {
+    let corpo = ``;
 
-    const converted = generatedNumbers.map((item: any[], index1: any) => {
-      mountedBody = mountedBody.concat(`\n`)
-      item.map((numberArray: string, index2: number) => {
+    const converter = geradorNumeros.map((item: any[], index1: any) => {
+      corpo = corpo.concat(`\n`)
+      item.map((numeroArray: string, index2: number) => {
         if (index2 < 6) {
-          return mountedBody = mountedBody.concat('|' + numberArray + '|');
+          return corpo = corpo.concat('' + numeroArray + '');
         }
 
       })
 
     })
 
-    return mountedBody;
+    return corpo;
 
   }
 
-  function verifyMatchingValues(values: any) {
+  function verificarValor(values: any) {
     let auxArray: any[] = [];
     let matches = 0;
     values.map((value: any[], index: any) => {
       value.map((itemOfArray, index) => {
 
-        if (String(itemOfArray) === String(sortedValues[`value${index}`])) {
+        if (String(itemOfArray) === String(sorteioValor[`value${index}`])) {
           matches++;
 
         }
@@ -111,21 +111,21 @@ export default function App() {
     })
    console.log(auxArray);
    
-    showAllRegisterOnGameTable();
+    registarJogosanTabela();
     return auxArray;
   }
 
   function conference() {
 
-    let arrayOfAccerts = verifyMatchingValues(generatedNumbers)
+    let acertos = verificarValor(geradorNumeros)
 
-    let gameMessage = '';
+    let mensagemJogo = '';
     let quadra = 0;
     let quina = 0;
     let sena = 0;
 
-    arrayOfAccerts.forEach((element, index) => {
-      gameMessage += `Jogo [${index + 1}] houveram [${element}] acerto(s) \n`
+    acertos.forEach((element, index) => {
+      mensagemJogo += `Jogo [${index + 1}] houveram [${element}] acerto(s) \n`
       if (Number(element) === 4) {
         quadra++;
       }
@@ -138,32 +138,30 @@ export default function App() {
 
 
     });
-    showAllRegisterOnGameTable();
-    alert(gameMessage + '\n' +
+    registarJogosanTabela();
+    alert(mensagemJogo + '\n' +
       "Jogo(s) ganho(s) com a sena : " + sena + "\n" +
       "Jogo(s) ganho(s) com a quina : " + quina + "\n" +
       "Jogo(s) ganho(s) com a quadra : " + quadra + "\n"
     )
   }
-  async function saveGamesOnDatabase(data: any) {
-    gameServiceDatabase.dropTable();
-    gameServiceDatabase.createTable();
+  async function salvarJogo(data: any) {
+    jogoDatabase.dropTable();
+    jogoDatabase.createTable();
 
     for (let game of data) {
 
-      return gameServiceDatabase.create({ sortedNumbers: game })
-      .then(id => console.log('Jogo created with id: ' + id))
+      return jogoDatabase.create({ sortedNumbers: game })
+      .then(id => console.log('Jogo Criado com o ID: ' + id))
       .catch(err => console.log(err))
        
     }
-
-
   }
 
-  //Consta erro mas funciona
-  function showAllRegisterOnGameTable() {
+  
+  function registarJogosanTabela() {
     {
-      gameServiceDatabase.all()
+      jogoDatabase.all()
         .then(
           game => game.forEach((register: any) => console.log(`id:${register.id}, values:${register.sortedNumbers}`))
         )
@@ -173,30 +171,40 @@ export default function App() {
   return (
     <ScrollView key="scroolView">
       <View style={styles.container} key="17">
-        <Text key="11">Insira a quantidad de Jogos</Text>
+      <Text style={styles.title}> Gerador da Mega-Sena</Text>
+        <Text key="11">Insera a quantidade de jogos</Text>
         <TextInput
           key="12"
           keyboardType="decimal-pad"
           style={styles.input}
-          onChangeText={(e) => { setNumberOfGames(Number(e)) }}
-          value={String(numberOfGames)}
+          onChangeText={(e) => { setnumeroGames(Number(e)) }}
+          value={String(numeroGames)}
         />
-        <Button key="13" title="Gerar Número" onPress={(e) => { handleClickSorterButton(Number(numberOfGames)) }} />
-
+        <Button
+         key="13"  
+         title="Gerar Número" 
+         color="#6C7B8B"
+         onPress={(e) => { clickButton(Number(numeroGames)) }}
+          />
+          <Text>
+            insira um E-mail
+            </Text>
         <TextInput
+        
         key="14"
           style={styles.inputEmail}
-          onChangeText={(e) => { setEmailTo(e) }}
-          value={String(emailTo)}
+          onChangeText={(e) => { setemail(e) }}
+          value={String(email)}
         />
-        <Button title="Enviar Email" key="15" onPress={(e) => {
-          const body = functionTransformObject()
-          sendEmail(emailTo, 'Números Mega Sena', body)
+        <Button title="Enviar Email" key="15" color="#6C7B8B"
+         onPress={(e) => {
+          const body = transObjeto()
+          sendEmail(email, 'Números Mega Sena', body)
         }} />
         <StatusBar key="16" style="auto" />
       </View>
-      <View key="sortedView">
-        {generatedNumbers.map((item: any, index: number) => (
+      <View key="VisualizarSorteio">
+        {geradorNumeros.map((item: any, index: number) => (
           <View style={styles.viewSorted} key={index}>
             {
               item.map((value: any, secondIndex: number) => {
@@ -205,16 +213,12 @@ export default function App() {
                     <View key={secondIndex} >
                       <Text key={secondIndex + index}
                         style={
-                          {
-                            backgroundColor: (index % 2) === 0 ? '#128C80' : '#16BAA9',
+                          {  
                             margin: 5,
-                            minWidth: 50,
-                            maxWidth: 50,
-                            minHeight: 50,
+                            minWidth: 10,
                             maxHeight: 50,
                             textAlign: 'center',
-                            borderRadius: 5,
-                            padding: 13,
+                            
                           }
                         }
                       >
@@ -247,58 +251,60 @@ export default function App() {
             style={styles.sortedInput}
             keyboardType="number-pad"
             onChangeText={(e) => {
-              setSortedValues({ ...sortedValues, value0: e })
+              setsorteioValor({ ...sorteioValor, value0: e })
             }}
             key="4"
-            value={String(sortedValues.value0)}
+            value={String(sorteioValor.value0)}
           />
           <TextInput
             style={styles.sortedInput}
             keyboardType="number-pad"
             onChangeText={(e) => {
-              setSortedValues({ ...sortedValues, value1: e })
+              setsorteioValor({ ...sorteioValor, value1: e })
             }}
             key="5"
-            value={String(sortedValues.value1)}
+            value={String(sorteioValor.value1)}
           />
           <TextInput
             style={styles.sortedInput}
             keyboardType="number-pad"
             onChangeText={(e) => {
-              setSortedValues({ ...sortedValues, value2: e })
+              setsorteioValor({ ...sorteioValor, value2: e })
             }}
             key="6"
-            value={String(sortedValues.value2)}
+            value={String(sorteioValor.value2)}
           />
           <TextInput
             style={styles.sortedInput}
             keyboardType="number-pad"
             onChangeText={(e) => {
-              setSortedValues({ ...sortedValues, value3: e })
+              setsorteioValor({ ...sorteioValor, value3: e })
             }}
             key="7"
-            value={String(sortedValues.value3)}
+            value={String(sorteioValor.value3)}
           />
           <TextInput
             style={styles.sortedInput}
             keyboardType="number-pad"
             onChangeText={(e) => {
-              setSortedValues({ ...sortedValues, value4: e })
+              setsorteioValor({ ...sorteioValor, value4: e })
             }}
             key="8"
-            value={String(sortedValues.value4)}
+            value={String(sorteioValor.value4)}
           />
           <TextInput
             style={styles.sortedInput}
             keyboardType="number-pad"
             onChangeText={(e) => {
-              setSortedValues({ ...sortedValues, value5: e })
+              setsorteioValor({ ...sorteioValor, value5: e })
             }}
             key="9"
-            value={String(sortedValues.value5)}
+            value={String(sorteioValor.value5)}
           />
         </View>
-        <Button title="Verificar Numeros" key="10" onPress={(e) => {
+        <Button title="Verificar Jogos"
+        color="#6C7B8B"
+         key="10" onPress={(e) => {
           conference()
         }} />
       </View>
@@ -344,14 +350,21 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   sortedInput: {
+    backgroundColor: '#CAE1FF',
     height: 60,
     width: 60,
     margin: 4,
-    borderWidth: 4,
+    borderWidth: 1,
     padding: 10,
-    borderRadius: 50,
+    borderRadius: 20,
     textAlign: 'center',
     fontWeight: 'bold',
     fontSize: 20,
   },
+
+  title: {
+    paddingVertical: 100,
+    fontSize: 32,
+    fontWeight: 'bold',
+  }
 });
---
+++ JogoDatabase.ts
@@ -0,0 +1,171 @@
+import db from "./database" 
+
+
+db.transaction((tx: { executeSql: (arg0: string) => void; }) => {
+ 
+  const p = tx.executeSql(
+    "crie tabela se nao existirem jogos;"
+  );
+
+  console.log("aaaa", db);
+  
+
+});
+
+
+const create = (obj: { sortedNumbers: any; }) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx: any) => {
+      
+      tx.executeSql(
+        "INSERT INTO Games (sortedNumbers) values (?);",
+        [obj.sortedNumbers],
+        
+        (_: any, { rowsAffected, insertId }: any) => {
+          if (rowsAffected > 0) resolve(insertId);
+          else reject("Error inserting obj: " + JSON.stringify(obj)); 
+        },
+        (_: any, error: any) => reject(error) 
+      );
+    });
+  });
+};
+
+
+const update = (id: string, obj: { sortedNumbers: any; }) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx: any) => {
+    
+      tx.executeSql(
+        "UPDATE Games SET sortedNumbers=? WHERE id=?;",
+        [obj.sortedNumbers],
+        
+        (_: any, { rowsAffected }: any) => {
+          if (rowsAffected > 0) resolve(rowsAffected);
+          else reject("Error updating obj: id=" + id); 
+        },
+        (_: any, error: any) => reject(error) 
+      );
+    });
+  });
+};
+
+
+const find = (id: string) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx:any) => {
+      
+      tx.executeSql(
+        "SELECT * FROM Games WHERE id=?;",
+        [id],
+        
+        (_: any, { rows }: any) => {
+          if (rows.length > 0) resolve(rows._array[0]);
+          else reject("Obj not found: id=" + id); 
+        },
+        (_: any, error: any) => reject(error) 
+      );
+    });
+  });
+};
+
+
+const findBysortedNumbers = (sortedNumbers: string) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx:any) => {
+      
+      tx.executeSql(
+        "SELECT * FROM Games WHERE sortedNumbers LIKE ?;",
+        [sortedNumbers],
+       
+        (_: any, { rows }: any) => {
+          if (rows.length > 0) resolve(rows._array);
+          else reject("Obj not found: sortedNumbers=" + sortedNumbers); 
+        },
+        (_: any, error: any) => reject(error) 
+      );
+    });
+  });
+};
+
+
+const all = () => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx: any) => {
+      
+      tx.executeSql(
+        "SELECT * FROM Games;",
+        [],
+       
+        (_: any, { rows }: any) => resolve(rows._array),
+        (_: any, error: any) => reject(error) 
+      );
+    });
+  });
+};
+
+
+const remove = (id: any) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx: any) => {
+      
+      tx.executeSql(
+        "exclusão de jogos;",
+        [id],
+        
+        (_: any, { rowsAffected }: any) => {
+          resolve(rowsAffected);
+        },
+        (_: any, error: any) => reject(error) 
+      );
+    });
+  });
+};
+
+
+
+
+
+ const createTable = () => {
+
+  return new Promise((resolve, reject) => {
+    db.transaction((tx:any) => {
+      
+      tx.executeSql(
+        "crie tabela se nao existirem jogos",
+        [],
+        
+        (_: any, { rows }: any) => resolve(rows._array),
+        (_: any, error: any) => reject(error) 
+      );
+    });
+  });
+};
+
+const dropTable = () => {
+
+  return new Promise((resolve, reject) => {
+    db.transaction((tx: any) => {
+      
+      tx.executeSql(
+        "DROP TABLE Games;",
+        [],
+        
+        (_: any, { rows }: any) => resolve(rows._array),
+        (_: any, error: any) => reject(error) 
+      );
+    });
+  });
+
+};
+
+export default {
+  createTable,
+  create,
+  update,
+  find,
+  findBysortedNumbers,
+  all,
+  remove,
+  dropTable,
+};
